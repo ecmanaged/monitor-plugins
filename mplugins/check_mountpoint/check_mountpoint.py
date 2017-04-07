a1a075e05bc645188a4ba0a0b1412f64523bd028
Arindam Choudhury <arindam@live.com>
2017-04-07 12:11:02 +0200

    check mountpoint

---
+++ mplugins/check_mountpoint/check_mountpoint.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'plugins'))
+
+from __mplugin import MPlugin
+from __mplugin import OK, CRITICAL
+
+class CheckMountPoint(MPlugin):
+    def run(self):
+        mountpoint = self.config.get('mountpoint')
+
+        if os.path.ismount(mountpoint):
+            self.exit(OK, message="mounted")
+        else:
+            self.exit(CRITICAL, message='not mounted')
+
+if __name__ == '__main__':
+    monitor = CheckMountPoint()
+    monitor.run()
+    
