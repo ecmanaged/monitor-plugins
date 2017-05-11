36b3bbae2c627e0f78a0412abfea25fb9014276f
Arindam Choudhury <arindam@live.com>
2017-05-11 16:56:28 +0200

    support for old version

--- mplugins/check_varnish/varnish_status.py
+++ mplugins/check_varnish/varnish_status.py
@@ -19,92 +19,165 @@ class VarnishStatus(MPlugin):
             shell=True,
             stdout=subprocess.PIPE).stdout.read()
 
-        if not raw_data:
-            self.exit(CRITICAL, message='Cannot get status from varnish')
+        if raw_data:
+            data = self._parse_data(raw_data)
 
-        data = self._parse_data(raw_data)
+            counter_data = [
+                'MAIN.sess_conn',
+                'MAIN.sess_drop',
+                'MAIN.sess_fail',
+                'MAIN.cache_hit',
+                'MAIN.cache_miss',
+                'MAIN.backend_conn',
+                'MAIN.backend_unhealthy',
+                'MAIN.backend_fail',
+                'MAIN.backend_toolate',
+                'MAIN.threads_created',
+                'MAIN.threads_destroyed',
+                'MAIN.threads_failed',
+                'MGT.child_start',
+                'MGT.child_exit',
+                'MGT.child_stop',
+                'MGT.child_died',
+                'MGT.child_dump',
+                'MGT.child_panic'
+            ]
 
-        counter_data = [
-            'MAIN.sess_conn',
-            'MAIN.sess_drop',
-            'MAIN.sess_fail',
-            'MAIN.cache_hit',
-            'MAIN.cache_miss',
-            'MAIN.backend_conn',
-            'MAIN.backend_unhealthy',
-            'MAIN.backend_fail',
-            'MAIN.backend_toolate',
-            'MAIN.threads_created',
-            'MAIN.threads_destroyed',
-            'MAIN.threads_failed',
-            'MGT.child_start',
-            'MGT.child_exit',
-            'MGT.child_stop',
-            'MGT.child_died',
-            'MGT.child_dump',
-            'MGT.child_panic'
-        ]
+            gauge_data = [
+                'MAIN.threads',
+                'MAIN.threads_limited',
+                'MAIN.thread_queue_len'
+            ]
 
-        gauge_data = [
-            'MAIN.threads',
-            'MAIN.threads_limited',
-            'MAIN.thread_queue_len'
-        ]
+            tmp_counter = {}
+            for idx in counter_data:
+                try:
+                    tmp_counter[idx] = int(data.get(idx, 0))
+                except:
+                    tmp_counter[idx] = data.get(idx, 0)
 
-        tmp_counter = {}
-        for idx in counter_data:
-            try:
-                tmp_counter[idx] = int(data.get(idx, 0))
-            except:
-                tmp_counter[idx] = data.get(idx, 0)
+            tmp_counter = self.counters(tmp_counter, 'varnish')
 
-        tmp_counter = self.counters(tmp_counter, 'varnish')
+            tmp_gauge = {}
+            for idx in gauge_data:
+                try:
+                    tmp_gauge[idx] = int(data.get(idx, 0))
+                except:
+                    tmp_gauge[idx] = data.get(idx, 0)
 
-        tmp_gauge = {}
-        for idx in gauge_data:
-            try:
-                tmp_gauge[idx] = int(data.get(idx, 0))
-            except:
-                tmp_gauge[idx] = data.get(idx, 0)
+            data = tmp_counter.copy()
+            data.update(tmp_gauge)
 
-        data = tmp_counter.copy()
-        data.update(tmp_gauge)
-
-        metrics = {
-            'session stats': {
-                'connection': data['MAIN.sess_conn'],
-                'drop': data['MAIN.sess_drop'],
-                'fail': data['MAIN.sess_fail'],
-            },
-            'cache stats': {
-                'hits': data['MAIN.cache_hit'],
-                'misses': data['MAIN.cache_miss']
-            },
-            'backend connection stats': {
-                'success': data['MAIN.backend_conn'],
-                'not attempted': data['MAIN.backend_unhealthy'],
-                'failures': data['MAIN.backend_fail'],
-                'closed': data['MAIN.backend_toolate']
-            },
-            'thread stats': {
-                'threads': data['MAIN.threads'],
-                'hit max': data['MAIN.threads_limited'],
-                'created': data['MAIN.threads_created'],
-                'destroyed': data['MAIN.threads_destroyed'],
-                'creation failed': data['MAIN.threads_failed'],
-                'session queue': data['MAIN.thread_queue_len']
-            },
-            'child processes stat': {
-                'started': data['MGT.child_start'],
-                'normal exit': data['MGT.child_exit'],
-                'unexpected exit': data['MGT.child_stop'],
-                'died (signal)': data['MGT.child_died'],
-                'core dumped': data['MGT.child_dump'],
-                'panic': data['MGT.child_panic']
+            metrics = {
+                'session stats': {
+                    'connection': data['MAIN.sess_conn'],
+                    'drop': data['MAIN.sess_drop'],
+                    'fail': data['MAIN.sess_fail'],
+                },
+                'cache stats': {
+                    'hits': data['MAIN.cache_hit'],
+                    'misses': data['MAIN.cache_miss']
+                },
+                'backend connection stats': {
+                    'success': data['MAIN.backend_conn'],
+                    'not attempted': data['MAIN.backend_unhealthy'],
+                    'failures': data['MAIN.backend_fail'],
+                    'closed': data['MAIN.backend_toolate']
+                },
+                'thread stats': {
+                    'threads': data['MAIN.threads'],
+                    'hit max': data['MAIN.threads_limited'],
+                    'created': data['MAIN.threads_created'],
+                    'destroyed': data['MAIN.threads_destroyed'],
+                    'creation failed': data['MAIN.threads_failed'],
+                    'session queue': data['MAIN.thread_queue_len']
+                },
+                'child processes stat': {
+                    'started': data['MGT.child_start'],
+                    'normal exit': data['MGT.child_exit'],
+                    'unexpected exit': data['MGT.child_stop'],
+                    'died (signal)': data['MGT.child_died'],
+                    'core dumped': data['MGT.child_dump'],
+                    'panic': data['MGT.child_panic']
+                }
             }
-        }
 
-        self.exit(OK, data, metrics)
+            self.exit(OK, data, metrics)
+
+
+        raw_data = subprocess.Popen(
+            'varnishstat -1 -x',
+            shell=True,
+            stdout=subprocess.PIPE).stdout.read()
+
+        if raw_data:
+            import xml.etree.ElementTree as ET
+
+            root = ET.fromstring(raw_data)
+
+            data = {}
+
+            for stat in root.findall('stat'):
+                name = None
+                if stat.find('type') is not None:
+                    name = '{}.{}.{}'.format(
+                        stat.find('type').text,
+                        stat.find('ident').text,
+                        stat.find('name').text
+                    )
+                else:
+                    name = stat.find('name').text
+
+                if name:
+                    data[name] = int(stat.find('value').text)
+
+            counter_data = [
+                'client_conn',
+                'client_drop',
+                'client_req',
+
+                'cache_hit',
+                'cache_miss',
+
+                'backend_conn',
+                'backend_unhealthy',
+                'backend_fail',
+                'backend_toolate'
+            ]
+
+            tmp_counter = {}
+            for idx in counter_data:
+                try:
+                    tmp_counter[idx] = int(data.get(idx, 0))
+                except:
+                    tmp_counter[idx] = data.get(idx, 0)
+
+            tmp_counter = self.counters(tmp_counter, 'varnish')
+
+            data = tmp_counter.copy()
+
+            metrics = {
+                'client stats': {
+                    'connection': data['client_conn'],
+                    'drop': data['client_drop'],
+                    'request': data['client_req'],
+                },
+                'cache stats': {
+                    'hits': data['cache_hit'],
+                    'misses': data['cache_miss']
+                },
+                'backend connection stats': {
+                    'success': data['backend_conn'],
+                    'not attempted': data['backend_unhealthy'],
+                    'failures': data['backend_fail'],
+                    'closed': data['backend_toolate']
+                },
+            }
+
+            self.exit(OK, data, metrics)
+
+        else:
+            self.exit(CRITICAL, message='Cannot get status from varnish')
 
     def _parse_data(self, raw):
         parsed = {}
